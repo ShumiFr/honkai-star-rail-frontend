148370f4d9a07813a48355769128e13ce6739417
Gagnaire Pauline <gagnaire.pauline@gmail.com>
2025-02-04 11:53:22 +0100

    Ajout de la bonne route backend

--- src/api/teamApi.js
+++ src/api/teamApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:10000/api";
+const API_URL = "https://honkai-star-rail-backend.onrender.com/api";
 
 export const saveTeam = async (uid, teamData) => {
   try {
