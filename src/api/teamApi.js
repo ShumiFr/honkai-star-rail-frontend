b09bc7584749e6a4df9fe7c8e6299ae8c97f1fca
Gagnaire Pauline <gagnaire.pauline@gmail.com>
2025-02-16 17:33:11 +0100

    changement de l'url

--- src/api/teamApi.js
+++ src/api/teamApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api"; // https://honkai-star-rail-backend.onrender.com/api
+const API_URL = "https://honkai-star-rail-backend.onrender.com/api";
 
 export const saveTeam = async (uid, teamData) => {
   try {
